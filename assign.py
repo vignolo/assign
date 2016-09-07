f68efc44888987432cc66bd214f50c50a677c3b6
Horacio Duran <horacio.duran@canonical.com>
2016-09-07 20:13:04 -0300

    Simplify, pythonize and add comments.

--- assign.py
+++ assign.py
@@ -3,9 +3,16 @@ __author__ = '@vignolo'
 import sys
 import random
 
-items = [[10,'Carne'], [10,'Bebida [Vino, Gaseosa (Aquarius Pomelo), Soda]'], [7,'Carbon'], [5,'Verdura [Tomate, Pimiento, Limon]'], [3, 'Pan (Celeste)'], [3,'Postre']]
+items = [[10,'Carne'], 
+        [10,'Bebida [Vino, Gaseosa (Aquarius Pomelo), Soda]'], 
+        [7,'Carbon'], 
+        [5,'Verdura [Tomate, Pimiento, Limon]'], 
+        [3, 'Pan (Celeste)'], 
+        [3,'Postre']]
 
 def process_args(options):
+    options = options[1:]
+    current_items = items[:]
 
     if '-help' in options:
         print "usage: assing.py [-p] [people..]"
@@ -13,50 +20,49 @@ def process_args(options):
         print "example: assign.py @Joey @Ross @Chandler"
         sys.exit()
 
+
     if '-p' in options:
-        items.append([2,'Picada'])
-        options.remove('-p')
+        current_items.append([2,'Picada'])
+        options = options[options.index("-p")+1:]
 
     if len(options) == 0:
         print "something is wrong. use -help"
         sys.exit()
 
-    return options
-
-def randomize_list(people):
-    random.shuffle(people)
-    return people
+    return options[:], current_items
 
 
-def construct_final_list(people):
-    assigned_list = {}
-    while people:
-        p = random.choice(people)
-        assigned_list[p] = []
-        people.remove(p)
-    return assigned_list
-
-def print_list(list):
+def print_list(cenadores):
     print " "
     print '~~ Cenadores ~~'
 
-    for key,value in list.iteritems():
+    for key,value in cenadores:
         print key, ": ", ", ".join([item[1] for item in value])
 
     print " "
 
+def fair_sort_key(element):
+    # put the people with least item assignment weight first.
+    assignment_weight = 0
+    for weight, _ in element[1]:
+        assignment_weight += weight
+    return assignment_weight
+
+def fair_put(assignment_list, item):
+    sorted_list = sorted(assignment_list, key=fair_sort_key)
+    # assign item to the person with least/lightest things.
+    assigned = assignment_list.index(sorted_list[0])
+    assignment_list[assigned][1].append(item)
+
+
 def main():
+    people, current_items = process_args(sys.argv)
+    people = zip(people, [list() for x in xrange(len(people))])
+    random.shuffle(people)
+    for item in current_items:
+        fair_put(people, item)
 
-    people = process_args(sys.argv[1:len(sys.argv) + 1])
-
-    final_list = construct_final_list(randomize_list(people))
-    print "final list: ", final_list
-
-    for item in items:
-        p = sorted(final_list.items(), key=lambda e: sum(i for i, j in e[1]))[0][0]
-        final_list[p].append(item)
-
-    print_list(final_list)
+    print_list(people)
 
 if __name__ == "__main__":
     main()
